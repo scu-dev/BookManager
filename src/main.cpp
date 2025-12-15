2bccb182b442058f8088a23f6528df5883b46478
LJM12914 <LJM12914@outlook.com>
2025-12-16 00:27:56 +0800

    feat: initial setup

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+﻿#include "utils.hpp"
+
+int main(int argc, char** argv) {
+    #if BM_WINDOWS
+        SetConsoleCP(CP_UTF8);
+        SetConsoleOutputCP(CP_UTF8);
+    #endif
+    
+    return 0;
+}
---
+++ src/utils.hpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <filesystem>
+#include <iostream>
+#include <string>
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) || defined(__NT__)
+    #define BM_WINDOWS 1
+    #include <windows.h> // IWYU pragma: keep
+#else
+    #define BM_UNIX 1
+    #include <unistd.h> // IWYU pragma: keep
+#endif
+
+namespace Toposort::Utils {
+    using std::string, std::filesystem::path, std::error_code, std::cout, std::endl;
+
+    inline string errorMessage;
+
+    [[nodiscard]] inline string getLastError() noexcept { return errorMessage; }
+    inline void setError(const string& err) noexcept { errorMessage = err; }
+
+    inline bool normalize(path& p) noexcept {
+        error_code ec;
+        p = absolute(p, ec);
+        if (ec) return false;
+        p = p.lexically_normal();
+        p.make_preferred();
+        return true;
+    }
+}
